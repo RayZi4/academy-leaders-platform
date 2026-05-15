6a19c187b2d07bd2f7b3c4207ec85ac38c24b0dc
Максим <garkin.2019@bk.ru>
2026-05-15 21:52:44 +0300

    Fix permissions and migrate order

--- start.sh
+++ start.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 cd /var/www/html
-echo "Clearing and caching config..."
+
+echo "Setting permissions..."
+touch /var/www/html/storage/logs/laravel.log
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
+echo "Clearing config cache..."
 php artisan config:clear
-php artisan config:cache
-echo "Caching routes..."
-php artisan route:cache
-echo "Caching views..."
-php artisan view:cache
+
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "Caching config, routes, views..."
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
 echo "Starting Apache..."
 apache2-foreground
